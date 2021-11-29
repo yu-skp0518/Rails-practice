c492d532cf0717bc5e79ca667c7520ca73a2580e
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-11-29 09:12:52 +0000

    [add] create-comment-function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+
+  has_many :comments
+
   validates :title, presence: true, length: { minimum: 3, message: 'Too short to post!' }
   validates :body, presence: true, length: { maximum: 300, message: 'Too long to post!' }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_160428) do
+ActiveRecord::Schema.define(version: 2021_11_29_084711) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
